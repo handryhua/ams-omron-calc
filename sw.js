f70bb714d1b0fc0a2a5ff10d0c07d8435984c9ed
handryhua <58896496+handryhua@users.noreply.github.com>
2026-04-29 13:40:55 +0700

    Update service worker cache version and URLs

--- sw.js
+++ sw.js
@@ -1,20 +1,23 @@
-const CACHE_NAME = "omron-app-v1";
+const CACHE_NAME = "omron-app-v2";
+
 const urlsToCache = [
-  "/ams-omron-calc/",
-  "/ams-omron-calc/index.html",
-  "/ams-omron-calc/manifest.json",
-  "/ams-omron-calc/icon-192.png",
-  "/ams-omron-calc/icon-512.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
